2dab4da977bb16c568522534bb8fe4a08be81052
RakibulRanak <43205114+RakibulRanak@users.noreply.github.com>
2020-05-03 01:57:25 +0600

    Add files via upload

---
+++ with_test.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve() {
+
+}
+
+int main() {
+    FastIO; solve();
+    return 0;
+}
